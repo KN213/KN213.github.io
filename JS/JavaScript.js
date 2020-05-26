6f4414eec8a2533a23379f7bd868bc37dbed914b
KN213 <65871042+KN213@users.noreply.github.com>
2020-05-26 23:59:28 +0300

    Add files via upload

--- JS/JavaScript.js
+++ JS/JavaScript.js
@@ -17,3 +17,34 @@ window.addEventListener('scroll', function() {
     arrowTop.hidden = (pageYOffset < document.documentElement.clientHeight);
 });
 
+var Light = document.querySelector('.Light')
+var Dark = document.querySelector('.Dark')
+
+var header = document.querySelectorAll('.header')
+var FlexboxItem = document.querySelectorAll('.FlexboxItem')
+var GridItem = document.querySelectorAll('.GridItem')
+var Body = document.querySelector('body')
+
+Light.addEventListener('click',function () {
+    Dark.style.display='block'
+    Light.style.display='none'
+    f(header,'background-color: yellow; color: black;')
+    f(FlexboxItem,'background-color: greenyellow;')
+    f(GridItem,'background-color: green; color: lightcyan;')
+    Body.style='background-image: url(./Images/back.jpeg);';
+})
+Dark.addEventListener('click',function () {
+    Light.style.display='block'
+    Dark.style.display='none'
+    f(header,'none')
+    f(FlexboxItem,'none')
+    f(GridItem,'none')
+    Body.style='none';
+})
+
+
+function f(a, b) {
+    for (var i = 0; i < a.length; i++) {
+        a[i].style = b;
+    }
+}
